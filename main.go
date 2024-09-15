a54d75bcc66bb8f2b3ec3b14f68f4bdb305c1741
stromokocur5 <gabrielkohut3@proton.me>
2024-09-15 20:57:04 +0200

    bleh :P

--- main.go
+++ main.go
@@ -3,13 +3,48 @@ package main
 import (
 	"anketovac/poll"
 	_ "anketovac/templates"
-	_ "net/http"
+
+	"context"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/joho/godotenv"
 )
 
+func Pool(dbpool *pgxpool.Pool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("dbpool", dbpool)
+	}
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	// defer dbpool.Close()
+
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'verify'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
 	r := gin.Default()
+	r.Use(Pool(dbpool))
+
 	r.GET("/:poll", poll.Show)
 	g_poll := r.Group("/poll")
 	{
--- poll/poll.go
+++ poll/poll.go
@@ -1,13 +1,28 @@
 package poll
 
 import (
-	"github.com/a-h/templ"
 	_ "github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
+
+	"context"
+	"fmt"
+	_ "log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	_ "github.com/joho/godotenv"
 )
 
 func Show(c *gin.Context) {
-	c.String(200, c.Param("poll"))
+	dbpool := c.MustGet("dbpool").(*pgxpool.Pool)
+	var greeting string
+	err := dbpool.QueryRow(context.Background(), "select 'verify'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+	c.String(200, greeting)
 }
 
 func Create(c *gin.Context) {}
